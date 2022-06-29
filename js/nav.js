9005fb74276da07c1442b1be0580d715de0b6591
Choi Gun-Ho <63341938+yozura@users.noreply.github.com>
2022-06-29 10:21:23 +0900

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,8 @@
-let nav = "<div class="pos-f-t">
-  <div class="collpase" id="navbarToggleExternalContent">
-     <div class="bg-dark p-4">
-       <h4 class="text-white">Collapsed content</h4>
-       <span class="text-muted">Toggleable via the navbar brand.</span>
-    </div>
-  </div> 
-  <nav class="navbar navbar-dark dg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-</div>";
+let nav = "";
+
+function getNav() {
+  let nav = document.getElementById('myNav');
+ 
+}
 
 document.write(nav);
